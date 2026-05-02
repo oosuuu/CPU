c1d404a354b50afa4456cf50e051b1fddf9d5af4
oosuuu <212678204@qq.com>
2026-05-02 20:01:43 +0800

    12.2 fin

--- MPI/Chapter12/12.2.cpp
+++ MPI/Chapter12/12.2.cpp
@@ -7,7 +7,6 @@ using namespace std;
 #define STEPS 10
 
 int main(int argc,char** argv){
-
     int myid,numprocs,i,j,n;
     int begin_row,end_row;
     int up,down,tag1,tag2;
@@ -22,7 +21,7 @@ int main(int argc,char** argv){
     MPI_Comm_rank(MPI_COMM_WORLD,&myid);
 
     cout<<"Process "<<myid<<" of "<<numprocs<<" is alive!"<<endl;
-  
+
     // Initialization
     for (i = 0; i < MS + 2; i++){
         for (j = 0; j < TS; j++){
@@ -89,27 +88,24 @@ int main(int argc,char** argv){
         end_row = MS - 1;
     }
 
-//Jacobi
+    MPI_Send_init(&b[end_row][1], TS-2, MPI_DOUBLE, down, tag1, MPI_COMM_WORLD, &req[0]);
+    MPI_Send_init(&b[begin_row][1], TS-2, MPI_DOUBLE, up, tag2, MPI_COMM_WORLD, &req[1]);
+    MPI_Recv_init(&a[0][1], TS-2, MPI_DOUBLE, up, tag1, MPI_COMM_WORLD, &req[2]);
+    MPI_Recv_init(&a[MS+1][1], TS-2, MPI_DOUBLE, down, tag2, MPI_COMM_WORLD, &req[3]);
+
     for (n = 0; n < STEPS; n++){
-        //caculate boudary
         for (j = 1; j < TS - 1; j++){
             b[begin_row][j] = (a[begin_row][j + 1] + a[begin_row][j - 1] + a[begin_row + 1][j] + a[begin_row - 1][j]) * 0.25;
             b[end_row][j] = (a[end_row][j + 1] + a[end_row][j - 1] + a[end_row + 1][j] + a[end_row - 1][j]) * 0.25;
         }
 
-        //non-block correspondence
-        MPI_Isend(&b[end_row][1], TS-2, MPI_DOUBLE, down, tag1, MPI_COMM_WORLD, &req[0]);
-        MPI_Isend(&b[begin_row][1], TS-2, MPI_DOUBLE, up, tag2, MPI_COMM_WORLD, &req[1]);
-        MPI_Irecv(&a[0][1], TS-2, MPI_DOUBLE, up, tag1, MPI_COMM_WORLD, &req[2]);
-        MPI_Irecv(&a[MS+1][1], TS-2, MPI_DOUBLE, down, tag2, MPI_COMM_WORLD, &req[3]);
+        MPI_Startall(4, req);
 
-        //caculate
         for (j = 1; j < TS-1; j++){
             for (i = begin_row + 1; i < end_row; i++){
                 b[i][j] = (a[i][j + 1] + a[i][j - 1] + a[i + 1][j] + a[i - 1][j]) * 0.25;
             }
         }
-
         //update
         for (i = begin_row; i < end_row + 1; i++){
             for (j = 1; j < TS-1; j++){
@@ -117,13 +113,12 @@ int main(int argc,char** argv){
             }
         }
 
-        //complete non_block correspondence
-        for (i = 0; i < 4; i++){
-            MPI_Wait(&req[i], &status[i]);
-        }
+        MPI_Waitall(4, req, status);
     }
-    
-    // print
+    for (i = 0; i < 4; i++){
+        MPI_Request_free(&req[i]);
+    }
+
     cout << "Process " << myid << ":" << endl;
    for (i = 1; i < MS + 1; i++)
     {
@@ -133,7 +128,6 @@ int main(int argc,char** argv){
         cout << "\n"
              << endl;
     }
-
-        MPI_Finalize();
-
+    
+    MPI_Finalize();
 }
